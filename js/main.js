6f727d2caf96d3390c5d5b9bbfd62a0eda91ddaa
Tatyana Shimonkina <sentyabova@mail.ru>
2023-01-03 18:02:56 +0300

    initial

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function()){
+    let showPopup = function(){
+        $('.popup-container').css('display','flex');
+    };
+    let closePopup = function(){
+        $('.popup-container').css('display','none');
+    };
+    let clearForm = function(){
+        $('.popup-container > input[type="text"]').val('');
+    };
+    
+}
